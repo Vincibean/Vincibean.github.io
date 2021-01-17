ab346b8696bed0e4fee3094e35d1dcdb77b4863b
Vincibean <sepensi@gmail.com>
2021-01-17 22:22:29 +0000

    reintroduced page_url; better context

--- site/site.hs
+++ site/site.hs
@@ -7,39 +7,42 @@ main = hakyll $ do
   match "assets/**" $ do
     route idRoute
     compile copyFileCompiler
-
   match ("about.markdown" .||. "contact.markdown") $ do
     route $ setExtension "html"
     compile
       $   pandocCompiler
-      >>= loadAndApplyTemplate "templates/page.html" (authorCtx <> siteCtx)
+      >>= loadAndApplyTemplate "templates/page.html" context
       >>= relativizeUrls
-
   match "posts/*" $ do
     route $ setExtension "html"
     compile
       $   pandocCompiler
-      >>= loadAndApplyTemplate "templates/post.html"
-                               (postCtx <> authorCtx <> siteCtx)
+      >>= loadAndApplyTemplate "templates/post.html" context
       >>= relativizeUrls
-
   match "pages/index.html" $ do
     route (constRoute "index.html")
     compile $ do
       posts <- recentFirst =<< loadAll "posts/*"
       let indexCtx =
-            listField "posts" (postCtx <> authorCtx) (return posts)
+            listField "posts" context (return posts)
               <> constField "title" "Home"
-              <> siteCtx
+              <> context
               <> defaultContext
-
       getResourceBody
         >>= applyAsTemplate indexCtx
         >>= loadAndApplyTemplate "templates/default.html" indexCtx
         >>= relativizeUrls
-
   match ("includes/*" .||. "templates/*") $ compile templateCompiler
 
+context :: Context String
+context = authorCtx <> postCtx <> siteCtx
+
+authorCtx :: Context String
+authorCtx =
+  constField "author" "Vincibean"
+    <> constField "author_image" "../assets/img/Vincibean.jpeg"
+    <> constField "author_bio"   ""
+    <> defaultContext
 
 postCtx :: Context String
 postCtx = dateField "date" "%B %e, %Y" <> defaultContext
@@ -50,10 +53,5 @@ siteCtx =
     <> constField "site_logo"        "../assets/img/icosaedron.svg"
     <> constField "site_description" "Just a Bunch of Tips"
     <> constField "site_cover"       "../assets/img/geometric.jpg"
+    <> constField "page_url"         "https://vincibean.github.io"
     <> defaultContext
-
-authorCtx :: Context String
-authorCtx =
-  constField "author" "Vincibean"
-    <> constField "author_image" "../assets/img/Vincibean.jpeg"
-    <> constField "author_bio"   ""
